223177a8f1f964fba39e981a4832736a80a54c33
kgrymd <kgroymd@gmail.com>
2023-08-20 23:55:53 +0900

    Reduxの練習

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './components/Navbar'
+import CartContainer from './components/CartContainer'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { calculateTotals } from './features/cart/CartSlice'
+import Modal from './components/Modal'
+
+function App() {
+  const dispatch = useDispatch();
+
+  const { cartItems } = useSelector(state => state.cart);
+
+  const { isOpen } = useSelector(state => state.modal);
+
+  useEffect(() => {
+    dispatch(calculateTotals())
+  }, [cartItems])
+
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <main>
+        {isOpen && <Modal />}
+        <Navbar />
+        <CartContainer />
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/cartItems.js
@@ -0,0 +1,32 @@
+const cartItems = [
+    {
+        id: 1,
+        title: "Next.js入門講座",
+        price: "12000",
+        img: "https://1.bp.blogspot.com/-tVeC6En4e_E/X96mhDTzJNI/AAAAAAABdBo/jlD_jvZvMuk3qUcNjA_XORrA4w3lhPkdQCNcBGAsYHQ/s1048/onepiece01_luffy.png",
+        amount: 1,
+    },
+    {
+        id: 2,
+        title: "MERNスタックで本格的なSNSアプリ構築講座",
+        price: "24000",
+        img: "https://1.bp.blogspot.com/-rzRcgoXDqEg/YAOTCKoCpPI/AAAAAAABdOI/5Bl3_zhOxm07TUGzW8_83cXMOT9yy1VJwCNcBGAsYHQ/s1041/onepiece02_zoro_bandana.png",
+        amount: 1,
+    },
+    {
+        id: 3,
+        title: "GraphQLとApollo入門講座",
+        price: "8900",
+        img: "https://1.bp.blogspot.com/-2ut_UQv3iss/X-Fcs_0oAII/AAAAAAABdD8/jrCZTd_xK-Y6CP1KwOtT_LpEpjp-1nvxgCNcBGAsYHQ/s1055/onepiece03_nami.png",
+        amount: 1,
+    },
+    {
+        id: 4,
+        title: "Three.jsでモダンウェブサイト構築講座",
+        price: "14000",
+        img: "https://1.bp.blogspot.com/-mZpzgXC1Sxk/YAOTCAKwWTI/AAAAAAABdOM/5B4hXli0KLU5N-BySHgjVbhZscKLSE-bQCNcBGAsYHQ/s1025/onepiece04_usopp_sogeking.png",
+        amount: 1,
+    },
+];
+
+export default cartItems;
